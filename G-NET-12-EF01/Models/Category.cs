2eb0874e6153d6d483df629487a74cab5a16ba46
Basmala Wael <basmalawael86@gmail.com>
2026-04-22 17:26:19 +0700

    Category

--- G-NET-12-EF01/Models/Category.cs
+++ G-NET-12-EF01/Models/Category.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace G_NET_12_EF01.Models
 {
     internal class Category
-    {
+    { 
         public int CategoryID { get; set; }
 
         public string CategoryName { get; set; }
